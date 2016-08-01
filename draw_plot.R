3c39f7766721690d054ef8dd37d6d5e36780362e
Dominic Smith <dosmith@cern.ch>
2016-08-01 10:03:57 +0100

    first commit

---
+++ draw_plot.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+# Dom Smith <dosmith@cern.ch>
+
+##__________________________________________________________________||
+argv <- commandArgs(trailingOnly = FALSE)
+
+
+main <- function()
+{
+
+  tblFileName <- 'tbl_ncomb_minChi.txt'
+
+  tblPath <- file.path('./', tblFileName)
+  if(!(file.exists(tblPath))) return()
+
+  tbl <- read.table(tblPath, header = TRUE)
+  tbl 
+}
+
+main()
