da42692fa585e64a9240c4f8fcaa26a2284924c8
Dominic Smith <dosmith@cern.ch>
2016-08-03 13:50:09 +0100

    Draw pie chart from dataframe

---
+++ draw_pie.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+# Dom Smith <dosmith@cern.ch>
+
+##__________________________________________________________________||
+argv <- commandArgs(trailingOnly = FALSE)
+
+##__________________________________________________________________||
+drawPie <- function(data, labels)
+{
+
+ png(file='smTotals_pie.png')
+ pie(data, labels) 
+ dev.off()
+ 
+}
+
+##__________________________________________________________________||
+main <- function()
+{
+
+ tblFileName <- 'tbl_ncomb_minChi.txt'
+ tblPath <- file.path('./',tblFileName)
+ if(!(file.exists(tblPath))) return() 	 
+
+ tbl <- read.table(tblPath, head = TRUE)
+
+ mainProcesses <- c("DYJetsToLL","DiBoson","QCD","SingleTop","TTJets","WJetsToLNu","Zinv")
+ df <- tbl[,mainProcesses]
+ df <- head(df,1)  
+
+ data <- as.numeric(df[1,])
+ drawPie(data, mainProcesses)
+
+}
+
+##__________________________________________________________________||
+main()
